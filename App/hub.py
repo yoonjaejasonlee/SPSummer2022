edafdbf8878ff6644bf2a1664177883b646e49a9
unknown <yoonj@fasoo.com>
2022-06-17 10:52:38 +0900

    changes

--- App/hub.py
+++ App/hub.py
@@ -2,10 +2,8 @@ from flask import jsonify
 import requests
 from prettytable import PrettyTable
 
-
 table = PrettyTable()
-table.field_names = ["User Name", "Created Date","Language", "Stars", "Forks","URL"]
-
+table.field_names = ["User Name", "Created Date", "Language", "Stars", "Forks", "URL"]
 
 user = 'yoonjaejasonlee'
 token = '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'
@@ -13,47 +11,30 @@ token = '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'
 authorization = f'token {token}'
 headers = {
     "Accept": "application/vnd.github.v3+json",
-    "Authorization" : authorization,
-    }
+    "Authorization": authorization,
+}
 i = 1
-while 1:
-    langs= "python"
-    stars = 3000
-    forks = 200
 
-    api_url = f"https://api.github.com/search/repositories?q=language:{langs}+stars:>={stars}+forks:>={forks}&page={i}&per_page={1}"
+langs = "python"
+stars = 3000
+forks = 200
 
-    #send get request
-    response = requests.get(api_url,auth=(user,token))
+api_url = "https://api.github.com/search/repositories?q=language:python+stars:%3E=100000+forks:%3E=30&page=1&per_page=10"
 
-    #get the json data
-    data =  response.json()
+# send get request
+response = requests.get(api_url)
 
-    for repository in data:
-        #name = repository["name"]
-        #created_date = repository["created_at"]
-        #language = repository["language"]
-        #stars = repository["stargazers_count"]
-        #forks = repository["forks"]
-        #url = repository["html_url"]
-        count = repository["total_count"]
-        
-        #table.add_row([name, created_date, language, stars,forks,url])
-        
+# get the json data
+data = response.json()
 
-    
-    #print(table)
+for repository in data["items"]:
+    names = repository["name"]
+    created_date = repository["created_at"]
+    language = repository["language"]
+    stars = repository["stargazers_count"]
+    forks = repository["forks"]
+    url = repository["html_url"]
 
-   #rl = requests.get("https://api.github.com/rate_limit", auth=(user,token))
+    table.add_row([names, created_date, language, stars, forks, url])
 
-    #data2 = rl.json()
-
-    print(count)
-
-
-    i +=1
-
-        
-
-
-    
+print(table)
--- App/sort.py
+++ App/sort.py
@@ -2,57 +2,67 @@ import os
 import lizard
 import requests
 from git.repo.base import Repo
+from prettytable import PrettyTable
 
+table = PrettyTable()
+table2 = PrettyTable()
+table.field_names = ["File", "CCN", "LOC" , "Cumulative LOC"]
+table2.field_names = ["name"]
 list_search = []
 i = 0
 CC_avg = 0
 total_lines = 0
 
-api_url = f"https://api.github.com/search/repositories?q=language:python+stars:%3E=30+forks:%3E=30&page=1&per_page=1"
+
+api_url = f"https://api.github.com/search/repositories?q=language:python+stars:%3E=100000+forks:%3E=30&page=1&per_page=3"
 
 response = requests.get(api_url)
 
 data = response.json()
 
 for s in data["items"]:
-    #urls = i["html_url"]
     name = s["full_name"]
+    hub = f"https://github.com/{name}"
+    temp_location = f"C:/Users/yoonj/Desktop/project-3-s22-yoonjaejasonlee-main/{name}"
 
-hub = f"https://github.com/{name}"
-temp_location = f"C:/Users/yoonj/Desktop/project-3-s22-yoonjaejasonlee-main/{name}"
-
-Repo.clone_from(hub, temp_location)  # 새로운 폴더 생성 그리고 그안에 리포 집어넣기
-
-
-def search(directory):
-    try:
-        filenames = os.listdir(directory)
-        for filename in filenames:
-            full_filename = os.path.join(directory, filename)
-            if os.path.isdir(full_filename):
-                search(full_filename)
-            else:
-                ext = os.path.splitext(full_filename)[-1]
-                if ext == '.py':
-                    list_search.append(full_filename)
-    except PermissionError:
-        pass
+    Repo.clone_from(hub, temp_location)  # 새로운 폴더 생성 그리고 그안에 리포 집어넣기
 
 
-search(temp_location)
+    def search(directory):
+        try:
+            filenames = os.listdir(directory)
+            for filename in filenames:
+                full_filename = os.path.join(directory, filename)
+                if os.path.isdir(full_filename):
+                    search(full_filename)
+                else:
+                    ext = os.path.splitext(full_filename)[-1]
+                    if ext == '.py':
+                        list_search.append(full_filename)
+        except PermissionError:
+            pass
 
-while i < len(list_search):
-    code = lizard.analyze_file(list_search[i])
-    CC = code.CCN
-    lines = code.nloc
-    CC_avg = CC_avg + CC
-    total_lines += lines
-    print("File:", code.filename)
-    print("Cyclomatic Complexity:", CC)
-    print("Lines of Code:", lines)
-    print("Cumulative number of LOC:", total_lines)
-    print("\n")
-    i += 1
 
+    search(temp_location)
+
+    while i < len(list_search):
+        code = lizard.analyze_file(list_search[i])
+        CC = code.CCN
+        lines = code.nloc
+        CC_avg = CC_avg + CC
+        total_lines += lines
+        table.add_row([code.filename, CC, lines, total_lines])
+        #print("File:", code.filename)
+        #print("Cyclomatic Complexity:", CC)
+        #print("Lines of Code:", lines)
+        #print("Cumulative number of LOC:", total_lines)
+        #print("\n")
+        i += 1
+
+    print(table)
+    table.clear_rows()
     if i == len(list_search):
         print("Average Cyclomatic Complexity for the Repo: ", CC_avg / len(list_search))
+        CC_avg = 0
+
+
